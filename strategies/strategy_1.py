82b990f3a545bac4b3144a5f90f5c715c93ec146
Sihyeong <ppark9553@gmail.com>
2021-02-04 16:53:24 +0900

    strategy 정리

--- strategies/strategy_1.py
+++ strategies/strategy_1.py
@@ -1,18 +1,31 @@
-from roboticks.strategy import Strategy
-from roboticks.event import SignalEvent
 import datetime
 import numpy as np
+from functools import partial
+
+from roboticks.strategy import Strategy
+from roboticks.event import SignalEvent
 
 
 class Strategy_1(Strategy):
-    def __init__(self, data_queue, port_queue, order_queue, strategy_universe, monitor_stocks,
+
+    def __init__(self, data_queue, port_queue, order_queue, strategy_name, strategy_universe, monitor_stocks,
                  sec_mem_name, sec_mem_shape, sec_mem_dtype):
+
+        self.strategy_name = strategy_name
+
         print('Strategy 1 started')
         super().__init__(data_queue, port_queue, order_queue, strategy_universe, monitor_stocks,
                          sec_mem_name, sec_mem_shape, sec_mem_dtype)
 
         self.long_window = 400
         self.short_window = 100
+
+        # BarStatic partial 함수
+        self.latest_bar_value = partial(self.get_latest_bar_value, data=self.sec_mem_array, symbol_table=self.SYMBOL_TABLE,
+                                        val_type='current_price', N=self.long_window)
+        self.latest_n_bars_value = partial(self.get_latest_n_bars_value, data=self.sec_mem_array, symbol_table=self.SYMBOL_TABLE,
+                                           val_type='current_price', N=self.long_window)
+        self.latest_bar_datetime = partial(self.get_latest_bar_datetime, data=self.sec_mem_array, symbol_table=self.SYMBOL_TABLE)
 
     def calc_signals(self):
         print('calculating signal...')
@@ -28,43 +41,43 @@ class Strategy_1(Strategy):
             cnt += 1
 
             for s in self.symbol_list:
-                bars = self.get_latest_n_bars_value(self.sec_mem_array, s, self.SYMBOL_TABLE, 'current_price', N=self.long_window)
-                bar_date = self.get_latest_bar_datetime(self.sec_mem_array, s, self.SYMBOL_TABLE)
+                bars = self.latest_n_bars_value(symbol=s)
+                bar_date = self.latest_bar_datetime(symbol=s)
+
                 if (bars is not None) and (bars.size > 0):
                     short_sma = np.mean(bars[-self.short_window:])
                     long_sma = np.mean(bars[-self.long_window:])
                     symbol = s
                     dt = datetime.datetime.utcnow()
-                    sig_dir = ""
 
-                    if short_sma > long_sma and bought[s]=="OUT":
+                    if short_sma > long_sma and bought[s] == "OUT":
                         print("LONG: %s" % bar_date)
                         sig_dir = "LONG"
-                        cur_price = self.get_latest_bar_value(self.sec_mem_array, s, self.SYMBOL_TABLE, 'current_price')
-                        signal = SignalEvent(1, symbol, dt, sig_dir, 1.0, cur_price)
+                        cur_price = self.latest_bar_value(symbol=s)
+                        signal = SignalEvent(self.strategy_name, symbol, dt, sig_dir, 1.0, cur_price)
                         bought[s] = 'LONG'
                         self.port_queue.put(signal)
 
-                    elif bought[s]=="OUT" and cnt==10:
+                    elif bought[s] == "OUT" and cnt == 10:
                         print("LONG: %s" % bar_date)
                         sig_dir = "LONG"
-                        cur_price = self.get_latest_bar_value(self.sec_mem_array, s, self.SYMBOL_TABLE, 'current_price')
-                        signal = SignalEvent(1, symbol, dt, sig_dir, 1.0, cur_price)
+                        cur_price = self.latest_bar_value(symbol=s)
+                        signal = SignalEvent(self.strategy_name, symbol, dt, sig_dir, 1.0, cur_price)
                         bought[s] = 'LONG'
                         self.port_queue.put(signal)
 
-                    elif bought[s]=="LONG" and cnt==30:
+                    elif bought[s] == "LONG" and cnt == 30:
                         print("SHORT: %s" % bar_date)
                         sig_dir = "EXIT"
-                        cur_price = self.get_latest_bar_value(self.sec_mem_array, s, self.SYMBOL_TABLE, 'current_price')
-                        signal = SignalEvent(1, symbol, dt, sig_dir, 1.0, cur_price)
+                        cur_price = self.latest_bar_value(symbol=s)
+                        signal = SignalEvent(self.strategy_name, symbol, dt, sig_dir, 1.0, cur_price)
                         bought[s] = 'OUT'
                         self.port_queue.put(signal)
 
                     elif short_sma < long_sma and bought[s]=="LONG":
                         print("SHORT: %s" % bar_date)
                         sig_dir = "EXIT"
-                        cur_price = self.get_latest_bar_value(self.sec_mem_array, s, self.SYMBOL_TABLE, 'current_price')
-                        signal = SignalEvent(1, symbol, dt, sig_dir, 1.0, cur_price)
+                        cur_price = self.latest_bar_value(symbol=s)
+                        signal = SignalEvent(self.strategy_name, symbol, dt, sig_dir, 1.0, cur_price)
                         bought[s] = 'OUT'
                         self.port_queue.put(signal)
